9236babe7d2bbdf5ec68b40b6c858cceaf622637
NehaArya <neha.arya@kiwitech.com>
2015-10-19 00:42:35 +0530

    second commit

---
+++ app/src/main/java/com/example/mac/demoapp/UI/Activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.mac.demoapp.UI.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mac on 19/10/15.
+ */
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
